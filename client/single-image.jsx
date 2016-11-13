73e8c68abd25974356ee7fe9cca524b58543a3ee
江湖之远 <freshyu2002@163.com>
2016-11-13 21:51:20 +0800

    upload file test passed

--- client/single-image.jsx
+++ client/single-image.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 export default class SingleImage extends React.Component {
+  removeFile(e) {
+    e.preventDefault();
+    Meteor.call('file.remove', this.props.file._id);
+  }
   render() {
     const file = this.props.file;
     const link = Images.findOne({_id: file._id}).link();
     return (
       <li>
         <a href={link+"?download=true"} download="{this.props.file.name}">
-          {this.props.file.name}</a>
+          {this.props.file.name}</a><br/>
+        <img src={link} height="300" width="200"/>
+        <button onClick={this.removeFile.bind(this)}>Remove</button>
       </li>
     )
   }
